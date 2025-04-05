36672259940ace4d58a1990d2d48bf669266cf51
Jason Turner <ubidzz@live.com>
2025-04-05 01:45:59 -0400

    Updated the WeedPricesChart.cs file

--- Schedule_1_Recipes/Weed/WeedPricesChart.cs
+++ Schedule_1_Recipes/Weed/WeedPricesChart.cs
@@ -12,50 +12,71 @@
 
             switch (SelectedWeed)
             {
+                case "92":
+                    items = new[] { "Banana Diamond" };
+                    break;
                 case "59":
-                    items = new[] {
-                    "California Smegma",
-                    };
+                    items = new[] { "California Smegma" };
                     break;
                 case "118":
-                    items = new[] {
-                    "Tokyo Ass",
-                    "Fruity Balls",
-                    "Wedding Thunderfuck"
-                    };
+                    items = new[] { "Fruity Balls", "Tokyo Ass", "Wedding Thunderfuck" };
                     break;
                 case "74":
-                    items = new[] {
-                    "Big Express",
-                    "Mega Diesel",
-                    "California Rhino"
-                    };
+                    items = new[] { "Big Express", "California Rhino", "Mega Diesel" };
                     break;
                 case "82":
-                    items = new[] {
-                    "Island Rhino",
-                    "Sweet Cheese",
-                    "Island Rhino"
-                    };
+                    items = new[] { "Island Rhino", "Sweet Cheese" };
                     break;
-                case "92":
-                    items = new[] {
-                    "Banana Diamond"
-                    };
+                case "85": 
+                    items = new[] { "Afghan Punch", "Afghan Kush" }; 
                     break;
                 case "101":
-                    items = new[] {
-                    "Fruity Cum",
-                    "Dark Ghost",
-                    "Hairy Shart",
-                    "Sweet Cum",
-                    "Wedding Express"
-                    };
+                    items = new[] { "Dark Ghost", "Fruity Cum", "Hairy Shart", "Sweet Cum", "Wedding Express" };
                     break;
-                case "111":
-                    items = new[] {
-                    "Death Urkle"
-                    };
+                case "115":
+                    items = new[] { "Purple Haze" };
+                    break;
+                case "128":
+                    items = new[] { "Grape Ape" };
+                    break;
+                case "135":
+                    items = new[] { "Sour Diesel" };
+                    break;
+                case "142":
+                    items = new[] { "Blue Dream" };
+                    break;
+                case "150":
+                    items = new[] { "Green Crack" };
+                    break;
+                case "158":
+                    items = new[] { "Jack Herer" };
+                    break;
+                case "165":
+                    items = new[] { "Northern Lights" };
+                    break;
+                case "173":
+                    items = new[] { "OG Kush" };
+                    break;
+                case "180":
+                    items = new[] { "Pineapple Express" };
+                    break;
+                case "188":
+                    items = new[] { "Sour OG" };
+                    break;
+                case "195":
+                    items = new[] { "Strawberry Cough" };
+                    break;
+                case "203":
+                    items = new[] { "Super Lemon Haze" };
+                    break;
+                case "210":
+                    items = new[] { "Tahoe OG" };
+                    break;
+                case "218":
+                    items = new[] { "Trainwreck" };
+                    break;
+                case "225":
+                    items = new[] { "White Widow" };
                     break;
             }
             if (items != null && items.Length > 0)
